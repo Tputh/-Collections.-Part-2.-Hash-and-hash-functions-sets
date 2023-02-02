d56bf4e3f065b3500d78aec3750e5d02413f5a1e
Имя <vitya.klyucherov@mail.ru>
2023-02-02 14:28:53 +0300

    Коллекции. Часть 2. Хеш и хеш-функции, множества

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Коллекции. Часть 2. Хеш и хеш-функции, множества");
+
+
+    }
+}
