7e05ced1a5f2865d9dac1d4db6409133970b3861
Имя <vitya.klyucherov@mail.ru>
2023-02-02 14:32:20 +0300

    Дз. Коллекции. Часть 2. Хеш и хеш-функции, множества

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Коллекции. Часть 2. Хеш и хеш-функции, множества");
+        Driver driver = new Driver("Иван");
+        Driver driver1 = new Driver("Иван");
 
-
+        Set<Driver> driverSet = new HashSet<>();
+        driverSet.add(driver);
+        driverSet.add(driver1);
+        System.out.println(driverSet);
     }
 }
